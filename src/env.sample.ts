1998d984a649808904e47caf8a2b70914985d2b5
Midhun P J <121170894+midhunfairfood@users.noreply.github.com>
2024-04-18 17:24:31 +0530

    Create env.sample.ts
    Signed-off-by: Midhun P J <121170894+midhunfairfood@users.noreply.github.com>

---
+++ src/env.sample.ts
@@ -0,0 +1,18 @@
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+export const environment = {
+  production: false,
+  baseUrl: 'https://v2.dev.api.fairfood.org/v2',
+  totpToken: 'your_totp_token',
+  gMapKey: 'your_google_map_key',
+  redirectUrl: 'https://trace-dev.fairfood.nl',
+  sentryDsn:
+    'https://cca4dec8e4a24478870ed3d957b07dc6@o1261458.ingest.sentry.io/4505079133175808',
+};
+
+

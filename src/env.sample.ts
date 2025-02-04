f9be441088c7f1a034b7832c871c27fa708195c2
Sudeesh <sudeesh@cied.in>
2025-02-04 10:55:20 +0100

    Re-add env.sample.ts after history removal

---
+++ src/env.sample.ts
@@ -0,0 +1,17 @@
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+export const environment = {
+  production: false,
+  baseUrl: 'https://v2.dev.api.fairfood.org/v2',
+  totpToken: 'your_totp_token',
+  gMapKey: 'your_google_map_key',
+  redirectUrl: 'https://trace-dev.fairfood.nl',
+  sentryDsn: 'your_sentry_dsn',
+};
+
+
